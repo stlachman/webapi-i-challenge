ac2e51325aefac539bae7bf60f64a9d2bc4352fe
Seth Lachman <lachmanseth17@gmail.com>
2019-05-24 15:30:46 -0400

    Added get users endpoint

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// implement your API here
+const express = require("express");
+const db = require("./data/db");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("It's alive!");
+});
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(hubs => {
+      res.status(200).json(hubs);
+    })
+    .catch(err => {
+      res.json({ error: err, message: "GET failed" });
+    });
+});
+
+server.listen(5000, () => console.log("API running on port 5000"));
