18acc912628af7c832021b5ef9d666a665200219
Seth Lachman <lachmanseth17@gmail.com>
2019-05-25 09:01:31 -0400

    Added get user by id.

--- index.js
+++ index.js
@@ -9,13 +9,38 @@ server.get("/", (req, res) => {
   res.send("It's alive!");
 });
 
+// Get All Users
+
 server.get("/api/users", (req, res) => {
   db.find()
     .then(hubs => {
       res.status(200).json(hubs);
     })
     .catch(err => {
-      res.json({ error: err, message: "GET failed" });
+      res.status(500).json({
+        error: "The users information could not be retrieved."
+      });
+    });
+});
+
+// Get user by id
+
+server.get("/api/users/:id", (req, res) => {
+  const userId = req.params.id;
+  db.findById(userId)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The user information could not be retrieved." });
     });
 });
 
